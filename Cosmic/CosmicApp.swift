594817682422ffd7fd1b3c9d481dfea92e402a77
Nizami Tagiyev <nizamion24s@gmail.com>
2022-06-08 15:56:25 +0500

    Initial Commit

---
+++ Cosmic/CosmicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicApp.swift
+//  Cosmic
+//
+//  Created by Nizami Tagiyev on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CosmicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
